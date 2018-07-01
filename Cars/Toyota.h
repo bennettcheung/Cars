b5d379489ab2c028e36f41124ae1ef98ef705510
Bennett Cheung <bennettcheung@yahoo.com>
2018-07-01 01:55:09 -0700

    Added Toyota class

---
+++ Cars/Toyota.h
@@ -0,0 +1,13 @@
+//
+//  Toyota.h
+//  Cars
+//
+//  Created by Bennett on 2018-07-01.
+//  Copyright © 2018 Bennett. All rights reserved.
+//
+
+#import "Car.h"
+
+@interface Toyota : Car
+
+@end
---
+++ Cars/Toyota.m
@@ -0,0 +1,18 @@
+//
+//  Toyota.m
+//  Cars
+//
+//  Created by Bennett on 2018-07-01.
+//  Copyright © 2018 Bennett. All rights reserved.
+//
+
+#import "Toyota.h"
+
+@implementation Toyota
+
+- (id)init
+{
+    return [self initWithModel:@"Prius"];
+}
+
+@end
